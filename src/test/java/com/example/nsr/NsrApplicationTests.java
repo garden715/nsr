afd1b2ecf4bea5a073279ab5af90ba73fb4a1f64
jungwon.seo <jungwon.seo@nhn.com>
2020-06-18 21:28:31 +0900

    init commit

---
+++ src/test/java/com/example/nsr/NsrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nsr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NsrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
