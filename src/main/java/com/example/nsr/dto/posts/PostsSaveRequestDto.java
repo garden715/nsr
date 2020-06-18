8418fc0a2af30354b6a95fd754c529eec12a924e
jungwon.seo <jungwon.seo@nhn.com>
2020-06-18 23:49:11 +0900

    add posts

---
+++ src/main/java/com/example/nsr/dto/posts/PostsSaveRequestDto.java
@@ -0,0 +1,21 @@
+package com.example.nsr.dto.posts;
+
+import com.example.nsr.domain.posts.Posts;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+
+    private String title;
+    private String content;
+    private String author;
+
+    public Posts toEntity() {
+        return Posts.builder().title(title).content(content).author(author).build();
+    }
+}
--- src/main/java/com/example/nsr/web/WebRestController.java
+++ src/main/java/com/example/nsr/web/WebRestController.java
@@ -1,13 +1,28 @@
 package com.example.nsr.web;
 
+import com.example.nsr.domain.posts.PostsRepository;
+import com.example.nsr.dto.posts.PostsSaveRequestDto;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
+
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello Jungwon";
+        return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+        postsRepository.save(dto.toEntity());
     }
 }
